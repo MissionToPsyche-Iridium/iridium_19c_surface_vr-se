b4a74ef7dd0693cf7254cfd8bb37561c3c6dbad8
Jose Grijalva <jlgrijal@asu.edu>
2025-02-01 21:27:03 -0700

    Task #215 roughly 80% completed. Still playing around with extra C# Scripts and other components to see what can be worked on for the next task.

---
+++ Assets/FingerTipFeedback.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Additional libraries for controller feedback.
+using UnityEngine.XR.Interaction.Toolkit;
+
+/// <summary>
+/// This script is used to provide feedback to the user when they touch the Wrist UI Menu with the finger tips on the right VR hand.
+/// </summary>
+
+[SerializeField] 
+public class FingerTipFeedback : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
