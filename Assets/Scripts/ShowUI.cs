9fb0c99861b547c521790a6877b5c13a96e26cb2
Iuliia Orellana <juliavladimiram@gmail.com>
2025-02-18 01:29:43 +0100

    T#244 and T#243 enchase info point design

--- Assets/Scripts/ShowUI.cs
+++ Assets/Scripts/ShowUI.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class ShowUI : MonoBehaviour
 {
-    public GameObject infoCanvas; // canvas with Psyche info
+    public GameObject infoCanvas; // canvas with info point info
+    public GameObject infoPanel; // canvas with Psyche facts
 
     void Start()
     {
@@ -17,8 +18,13 @@ public class ShowUI : MonoBehaviour
         UnityEngine.Debug.Log("Trigger Entered by: " + other.gameObject.name);
         if (other.CompareTag("Player")) // check if player entered
         {
-            UnityEngine.Debug.Log("Player entered trigger, showing UI."); // debug
-            infoCanvas.SetActive(true); // show canvas 
+            UnityEngine.Debug.Log("Player entered trigger."); // debug
+            if (infoPanel == null || !infoPanel.activeSelf) // check if info is shown
+            {
+                UnityEngine.Debug.Log("Showing infoCanvas.");
+                infoCanvas.SetActive(true); // show canvas
+            }
+             
         }
     }
 
@@ -28,6 +34,7 @@ public class ShowUI : MonoBehaviour
         {
             UnityEngine.Debug.Log("Player exited trigger, showing UI."); //debug
             infoCanvas.SetActive(false); // hide canvas
+            infoPanel.SetActive(false);
         }
     }
 }
