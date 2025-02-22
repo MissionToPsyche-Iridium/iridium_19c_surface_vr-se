e543524a962b3e73dbe58406e7f39f78332d3c16
Jose Grijalva <jlgrijal@asu.edu>
2025-02-22 02:30:40 -0700

    100% of Task #259 completed. Finally got all of the panels with the wrist UI Menu fixed and now that Decision Panel functions as intended.

--- Assets/Scripts/WristUIPauseManage.cs
+++ Assets/Scripts/WristUIPauseManage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.UI; // Import the Unity Engine UI library for the Button.
 using UnityEngine;
 
+
 using UnityEngine.InputSystem; // Import the Unity Engine Input System library for the InputAction.CallbackContext.
 using UnityEngine.SceneManagement; // Import the Unity Engine Scene Management library for the SceneManager.
 
@@ -21,31 +22,15 @@ public class WristUIPauseManage : MonoBehaviour
 
         if (decisionPanel != null)
             decisionPanel.SetActive(false);
-
-        // For now, trying to force trigger a button click for testing
-        // as the buttons on the main Wrist UI menu suddenly becoming completely
-        // unresponsive after implementing the DecisionPanel function.
-        Button[] testbuttons = wristPauseUI.GetComponentsInChildren<Button>();
-        foreach (Button btn in testbuttons)
-        {
-            Debug.Log("Found Button: " + btn.gameObject.name);
-        }
     }
-
 
     // Update is called once per frame
+    /*
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10f))
-        {
-            Debug.Log("XR Raycast hit: " + hit.collider.gameObject.name);
-        }
-        else
-        {
-            Debug.Log("XR Raycast is NOT hitting anything.");
-        }
+
     }
+    */
 
     /// <summary>
     /// A method to show the wrist pause UI when the X button on the left VR controller is pressed.
@@ -71,9 +56,9 @@ public class WristUIPauseManage : MonoBehaviour
             turnOnWristPauseUI = false;
             Time.timeScale = 1;
         } // If the boolean is false, turn on the wrist pause UI and set the time scale to 0.
-        else if (!turnOnWristPauseUI)
+        else
         {
-            wristPauseUI.SetActive(true);
+            wristPauseUI.SetActive(true);   
             turnOnWristPauseUI = true;
             Time.timeScale = 0;
         }
@@ -138,7 +123,6 @@ public class WristUIPauseManage : MonoBehaviour
         if (decisionPanel != null)
         {
             decisionPanel.SetActive(true);
-            wristPauseUI.SetActive(false); // Hide the wrist pause UI when you click the "Close Application" button
         }
     }
 
@@ -149,11 +133,9 @@ public class WristUIPauseManage : MonoBehaviour
     /// </summary>
     public void DoNotCloseApplication()
     {
-        Debug.Log("No button clicked - Returning to Main UI");
         if (decisionPanel != null)
         {
             decisionPanel.SetActive(false);
-            wristPauseUI.SetActive(true); // Show the wrist pause UI when you click the "No" button on the deicsion panel
         }
     }
 }
