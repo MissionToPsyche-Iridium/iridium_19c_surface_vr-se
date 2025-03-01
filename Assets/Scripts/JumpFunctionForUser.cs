e420f265fa5efc1a18c756d70a610dbdf9454042
Jose Grijalva <jlgrijal@asu.edu>
2025-02-28 19:54:02 -0700

    80% of Task #278 completed. Just finished my C# Script for Jumping but still need to test it and will do that tomorrow.

--- Assets/Scripts/JumpFunctionForUser.cs
+++ Assets/Scripts/JumpFunctionForUser.cs
@@ -5,13 +5,13 @@ using UnityEngine.InputSystem;
 
 public class JumpFunctionForUser : MonoBehaviour
 {
-
     [SerializeField] private InputActionProperty buttonForJumping;
-    [SerializeField] private float heightOfJump = 4f;
-    [SerializeField] private float controllerForCharacter;
+    [SerializeField] private float heightOfJump = 3f;
+    [SerializeField] private CharacterController controllerForCharacter;
     [SerializeField] private LayerMask layersForGround;
 
     private float gravityForMars = Physics.gravity.y; // To calculate how high we want to jump in Mars.
+    private Vector3 jumpMovement;
 
     /// <summary>
     /// This function is used to check with CheckSphere if the player is touching any of the ground layers or not, to ensure proper landing.
@@ -19,17 +19,32 @@ public class JumpFunctionForUser : MonoBehaviour
     private bool isLanded()
     {
         return Physics.CheckSphere(transform.position, 0.2f, layersForGround);
-    } 
-    
+    }
+
+    /// <summary>
+    /// This function is used to calculate the jump movement for the player in VR. The default Jump height is at 3.
+    /// </summary>
+    private void JumpInVR()
+    {
+        jumpMovement.y = Mathf.Sqrt(heightOfJump * -3f * gravityForMars); // Calculating the jump movement.
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Ensure the jumpMovement is initialized
+        jumpMovement = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (buttonForJumping.action.WasPressedThisFrame() && isLanded())
+        {
+            JumpInVR();
+        }
+
+        jumpMovement.y += gravityForMars * Time.deltaTime; // Calculating the gravity for the jump.
+        controllerForCharacter.Move(jumpMovement * Time.deltaTime); // Moving the player in the jumping motion.
     }
 }
