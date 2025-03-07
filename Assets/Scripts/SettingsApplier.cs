7af0a39b3d9c682e2ff4bbf3caff4862cfc652ac
EstevanP <eperez56@asu.edu>
2025-03-06 19:19:18 -0800

    Made it so audio doesn't stop playing when changing scenes

--- Assets/Scripts/SettingsApplier.cs
+++ Assets/Scripts/SettingsApplier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.SceneManagement;
 
 public class SettingsApplier : MonoBehaviour
 {
@@ -14,6 +15,22 @@ public class SettingsApplier : MonoBehaviour
     private Exposure _exposure; // The exposure of the volume
     private float _lightIntensityBase; //The base intensity of the light in the scene
     private float _lightIntensityRange; //The range of intensity of the light in the scene
+    private Scene _currentScene;
+    
+    public static SettingsApplier instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -25,6 +42,7 @@ public class SettingsApplier : MonoBehaviour
         _audioSources = FindObjectsOfType<AudioSource>();
         _volume = FindObjectsOfType<Volume>();
         _lightIntensityRange = 6;
+        _currentScene = SceneManager.GetActiveScene();
         
         // Apply audio
         if (_audioSources != null)
@@ -38,6 +56,7 @@ public class SettingsApplier : MonoBehaviour
                 foreach (var VARIABLE in _audioSources)
                 {
                     VARIABLE.volume = _prevVolume;
+                    VARIABLE.Play();
                 }
             }
         }
@@ -75,6 +94,32 @@ public class SettingsApplier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If the scene isn't the same scene and isn't on a menu scene find new audio sources
+        AudioSource[] tempAudioSources = GetComponentsInChildren<AudioSource>();
+
+        if (tempAudioSources.Length > 0) 
+        {
+            if (!isOnMenuScene())
+            {
+                foreach (var audioSource in tempAudioSources)
+                {
+                    audioSource.Stop();
+                }
+            }
+            else
+            {
+                foreach (var audioSource in tempAudioSources)
+                {
+                    if (!audioSource.isPlaying)
+                    {
+                        audioSource.Play();
+                    }
+                }
+            }
+        }
+
+        _currentScene = SceneManager.GetActiveScene();
+        
         // Updates Volume
         if (_audioSources != null)
         {
@@ -120,8 +165,27 @@ public class SettingsApplier : MonoBehaviour
             }
             
         }
+
+        
         
     }
+
+    public bool isOnMenuScene()
+    {
+        var answer = false;
+        var tempScene = SceneManager.GetActiveScene();
+        if (tempScene.Equals(SceneManager.GetSceneByName("AboutScreen"))
+            || tempScene.Equals(SceneManager.GetSceneByName("SettingsScreen"))
+            || tempScene.Equals(SceneManager.GetSceneByName("MarsMenu"))
+            || tempScene.Equals(SceneManager.GetSceneByName("MainMenuVR"))
+            || tempScene.Equals(SceneManager.GetSceneByName("CreditsScreen")))
+        {
+            answer = true;
+        }
+        
+        return answer;
+    }
+    
     
     /// <summary>
     /// This methods retrieves config project settings
--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -23,7 +24,7 @@ public class SettingsManager : MonoBehaviour
     private GameObject InteractionFeedbackSlider;
     
     private const string QualityPrefKey = "GraphicsQuality";
-
+    
     private void Start()
     {
         int savedQualityLevel = PlayerPrefs.GetInt(QualityPrefKey, QualitySettings.GetQualityLevel());
