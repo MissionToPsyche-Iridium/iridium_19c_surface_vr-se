c5f331f3d31f210a5227838b907c7d94dc954836
EstevanP <eperez56@asu.edu>
2025-03-21 22:43:38 -0700

    Add ability for Unity project to be built successfully
    Resolved Unity Build Errors.

--- Assets/Scripts/CustomSettings.cs
+++ Assets/Scripts/CustomSettings.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class CustomSettings : ScriptableObject
 {
+    // Path to save the custom config settings
+    private const string CustomSettingsPath = "Assets/Resources/CustomSettings.asset";
 
-    
-    internal static CustomSettings GetOrCreateSettings()
+#if UNITY_EDITOR
+    private static CustomSettings GetOrCreateSettings()
     {
-        var settings = AssetDatabase.LoadAssetAtPath<CustomSettings>(customSettingsPath);
-        if (settings == null)
-        {
-            settings = ScriptableObject.CreateInstance<CustomSettings>();
-            settings.isVRMode = true;
-            settings.brightness = 0.5f;
-            settings.volume = 0.5f;
-            settings.isVRMode = true;
-            settings.interactionFeedback = 0.5f;
-            AssetDatabase.CreateAsset(settings, customSettingsPath);
-            AssetDatabase.SaveAssets();
-        }
+        var settings = AssetDatabase.LoadAssetAtPath<CustomSettings>(CustomSettingsPath);
+        if (settings != null) return settings;
+        // Default variables and their initial values
+        settings = ScriptableObject.CreateInstance<CustomSettings>();
+        settings.brightness = 0.5f;
+        settings.volume = 0.5f;
+        settings.interactionFeedback = 0.5f;
+        AssetDatabase.CreateAsset(settings, CustomSettingsPath);
+        AssetDatabase.SaveAssets();
         return settings;
     }
 
@@ -28,72 +28,49 @@ public class CustomSettings : ScriptableObject
     {
         return new SerializedObject(GetOrCreateSettings());
     }
-    
-     public const string customSettingsPath = "Assets/Resources/CustomSettings.asset";
+#endif
 
-    [SerializeField]
-    private bool isVRMode;
-    public bool IsVRMode
-    {
-        get => isVRMode;
-        set
-        {
-            isVRMode = value;
-            Save();
-        }
-    }
-    
-    [SerializeField]
-    [Range(0,1)]
-    private float brightness;
+    [SerializeField, Range(0, 1)] private float brightness;
     public float Brighntness
     {
         get => brightness;
         set
         {
-            if (!Mathf.Approximately(brightness, value))
-            {
-                brightness = value;
-                Save();
-            }
+            if (Mathf.Approximately(brightness, value)) return;
+            brightness = value;
+            Save();
         }
-    
     }
-    
-    [SerializeField]
-    [Range(0,1)]
-    private float volume;
+
+    [SerializeField, Range(0, 1)] private float volume;
     public float Volume
     {
         get => volume;
         set
         {
-            if (Mathf.Approximately(volume, value)) return; // Doesn't change if it is about the right value
+            if (Mathf.Approximately(volume, value)) return;
             volume = value;
             Save();
         }
-    
     }
-    
-    [SerializeField]
-    [Range(0,1)]
-    private float interactionFeedback;
+
+    [SerializeField, Range(0, 1)] private float interactionFeedback;
     public float InteractionFeedback
     {
         get => interactionFeedback;
         set
         {
-            if (Mathf.Approximately(interactionFeedback, value)) return; // Doesn't change if it is about the right value
+            if (Mathf.Approximately(interactionFeedback, value)) return;
             interactionFeedback = value;
             Save();
         }
-    
     }
-    
-    // Centralized save methoda
+
     private void Save()
     {
-        EditorUtility.SetDirty(this); // Used to mark the asset as dirty so it gets saved
-        AssetDatabase.SaveAssets();  // Save changes to disk
+        #if UNITY_EDITOR
+                EditorUtility.SetDirty(this);
+                AssetDatabase.SaveAssets();
+        #endif
     }
 }
