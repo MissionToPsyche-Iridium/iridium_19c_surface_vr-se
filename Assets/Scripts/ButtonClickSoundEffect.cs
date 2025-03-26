005a20e9da6e49afddeff606a2b80ae4539e11a1
Jose Grijalva <jlgrijal@asu.edu>
2025-03-25 21:46:18 -0700

    30% of Task #301 Completed
    Just got started on creating a branch new C# Script source code for button clicking sound effects. Have not completed the necessary functions yet but left comments in them for ideas/plan on how to implement.

---
+++ Assets/Scripts/ButtonClickSoundEffect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// C# Script Description: This script is mean to be a simple implementation of 
+/// adding sound effects to button click events for each individual button.
+/// </summary>
+[RequireComponent(typeof(Button))] // This component makes sure that Button component is attached to the GameObject
+public class ButtonClickSoundEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        // Get the Button component attached to the GameObject
+    }
+
+    void PlayForSoundEffect()
+    {
+        // Play sound effect here
+    }
+}
