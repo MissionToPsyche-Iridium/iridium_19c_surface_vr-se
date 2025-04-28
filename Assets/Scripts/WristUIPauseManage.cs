c2b03cceeb55824c486176b23067d69afd25fe70
EstevanP <eperez56@asu.edu>
2025-04-27 20:49:34 -0700

    Update WristUIPauseManage.cs

--- Assets/Scripts/WristUIPauseManage.cs
+++ Assets/Scripts/WristUIPauseManage.cs
@@ -1,33 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI; // Import the Unity Engine UI library for the Button.
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
-using System.Linq; // Import the System Linq library for the Select method to handle the array of fast travel spots.
+// Import the Unity Engine UI library for the Button.
+// Import the System Linq library for the Select method to handle the array of fast travel spots.
 
+// Import the Unity Engine Input System library for the InputAction.CallbackContext.
 
-using UnityEngine.InputSystem; // Import the Unity Engine Input System library for the InputAction.CallbackContext.
-using UnityEngine.SceneManagement; // Import the Unity Engine Scene Management library for the SceneManager.
+// Import the Unity Engine Scene Management library for the SceneManager.
 
 public class WristUIPauseManage : MonoBehaviour
 {
     public GameObject wristPauseUI; // The wrist pause UI object.
-    public bool turnOnWristPauseUI = true; // A boolean to turn on the wrist pause UI when a specific button is pressed(X button on left VR controller).
+
+    public bool
+        turnOnWristPauseUI =
+            true; // A boolean to turn on the wrist pause UI when a specific button is pressed(X button on left VR controller).
+
     public GameObject decisionPanel; // The object for the decision panel to handle "Yes" or "No" buttons
 
     [Header("Component Settings for Fast Travel")]
-    public Transform vrUser;  // The Setting for assigning the XR Origin or Camera Rig prefab on the component setting.
+    public Transform vrUser; // The Setting for assigning the XR Origin or Camera Rig prefab on the component setting.
+
     public Transform[] extraTeleportSpots; // Optional manually assigned locations.
-                                           // Decided to implement Fast Travel function where it can be both manually and automatically assigned for scalability.
-    private Transform[] m_TeleportSpots; // Stores all valid travel points
+
     private int m_MostRecentIndex = -1; // Tracks the most recent fast travel spot teleported to.
+
+    // Decided to implement Fast Travel function where it can be both manually and automatically assigned for scalability.
+    private Transform[] m_TeleportSpots; // Stores all valid travel points
 
 
     // Start is called before the first frame update
 
     // Update: 02/19/2025 - Added the decisionPanel.SetActive(false)
     // to set the decision panel to false when the game starts.
-    void Start()
+    private void Start()
     {
         SetUpFastTravel();
 
@@ -35,7 +42,6 @@ public class WristUIPauseManage : MonoBehaviour
 
         if (decisionPanel != null)
             decisionPanel.SetActive(false);
-
     }
 
     // Update is called once per frame
@@ -47,19 +53,17 @@ public class WristUIPauseManage : MonoBehaviour
     */
 
     /// <summary>
-    /// A method to show the wrist pause UI when the X button on the left VR controller is pressed.
+    ///     A method to show the wrist pause UI when the X button on the left VR controller is pressed.
     /// </summary>
     /// <param name="context"></param>
     public void PressingWristUIButton(InputAction.CallbackContext context)
     {
-        if (context.performed)
-        {
-            ShowUIForWristPause();
-        }
+        if (context.performed) ShowUIForWristPause();
     }
 
     /// <summary>
-    /// A method to fully handle the wrist pause UI. If the boolean is true, turn off the wrist pause UI and set the time scale to 1.
+    ///     A method to fully handle the wrist pause UI. If the boolean is true, turn off the wrist pause UI and set the time
+    ///     scale to 1.
     /// </summary>
     public void ShowUIForWristPause()
     {
@@ -72,17 +76,16 @@ public class WristUIPauseManage : MonoBehaviour
         } // If the boolean is false, turn on the wrist pause UI and set the time scale to 0.
         else
         {
-            wristPauseUI.SetActive(true);   
+            wristPauseUI.SetActive(true);
             turnOnWristPauseUI = true;
             Time.timeScale = 0;
         }
     }
 
     /// <summary>
-    /// Description: Task #239 - Adding the additional function, which is to Return to the Title/Main Menu Scene.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 02/07/2025
+    ///     Description: Task #239 - Adding the additional function, which is to Return to the Title/Main Menu Scene.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 02/07/2025
     /// </summary>
     public void ReturnToMainMenu()
     {
@@ -90,10 +93,10 @@ public class WristUIPauseManage : MonoBehaviour
     }
 
     /// <summary>
-    /// Description: Task #258 - Adding the additional function, which is to Go to the Mars Site 3 Scene, from Mars Site 2 or Mars Site 1.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 02/15/2025
+    ///     Description: Task #258 - Adding the additional function, which is to Go to the Mars Site 3 Scene, from Mars Site 2
+    ///     or Mars Site 1.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 02/15/2025
     /// </summary>
     public void GoToMarsSite3()
     {
@@ -101,10 +104,10 @@ public class WristUIPauseManage : MonoBehaviour
     }
 
     /// <summary>
-    /// Description: Task #258 - Adding the additional function, which is to Go to the Mars Site 2 Scene, from Mars Site 3 or Mars Site 1.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 02/15/2025
+    ///     Description: Task #258 - Adding the additional function, which is to Go to the Mars Site 2 Scene, from Mars Site 3
+    ///     or Mars Site 1.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 02/15/2025
     /// </summary>
     public void GoToMarsSite2()
     {
@@ -112,10 +115,10 @@ public class WristUIPauseManage : MonoBehaviour
     }
 
     /// <summary>
-    /// Description: Task #258 - Adding the additional function, which is to Go to the Mars Site 1 Scene, from Mars Site 3 or Mars Site 2.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 02/15/2025
+    ///     Description: Task #258 - Adding the additional function, which is to Go to the Mars Site 1 Scene, from Mars Site 3
+    ///     or Mars Site 2.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 02/15/2025
     /// </summary>
     public void GoToMarsSite1()
     {
@@ -123,7 +126,7 @@ public class WristUIPauseManage : MonoBehaviour
     }
 
     /// <summary>
-    /// A method to exit the application completely once you click/touch the exit button on the wrist pause UI.
+    ///     A method to exit the application completely once you click/touch the exit button on the wrist pause UI.
     /// </summary>
     public void CloseApplication()
     {
@@ -133,40 +136,31 @@ public class WristUIPauseManage : MonoBehaviour
 
 
     /// <summary>
-    /// A method to display the decision panel when you click the "Close Application" button 
-    /// on the wrist pause UI.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 02/19/2025
+    ///     A method to display the decision panel when you click the "Close Application" button
+    ///     on the wrist pause UI.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 02/19/2025
     /// </summary>
     public void DisplayDecisionPanel()
     {
-        if (decisionPanel != null)
-        {
-            decisionPanel.SetActive(true);
-        }
+        if (decisionPanel != null) decisionPanel.SetActive(true);
     }
 
     /// <summary>
-    /// A method to close the application when you click the "Yes" button on the decision panel.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 02/19/2025
+    ///     A method to close the application when you click the "Yes" button on the decision panel.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 02/19/2025
     /// </summary>
     public void DoNotCloseApplication()
     {
-        if (decisionPanel != null)
-        {
-            decisionPanel.SetActive(false);
-        }
+        if (decisionPanel != null) decisionPanel.SetActive(false);
     }
 
     /// <summary>
-    /// A function for detecting and storing the fast travel spots manually and auto-detected objects
-    /// based on the "FastTravel" tag.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 03/06/2025
+    ///     A function for detecting and storing the fast travel spots manually and auto-detected objects
+    ///     based on the "FastTravel" tag.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 03/06/2025
     /// </summary>
     public void SetUpFastTravel()
     {
@@ -186,16 +180,11 @@ public class WristUIPauseManage : MonoBehaviour
 
         // Loop through the extra teleport spots and
         // auto travel spots to assign them to the teleportSpots array.
-        for (int i = 0; i < extraTeleportSpots.Length; i++)
-        {
-            m_TeleportSpots[i] = extraTeleportSpots[i];
-        }
+        for (int i = 0; i < extraTeleportSpots.Length; i++) m_TeleportSpots[i] = extraTeleportSpots[i];
 
         // Loop through the auto travel spots to assign them to the teleportSpots array.
         for (int i = 0; i < autoTravelSpots.Length; i++)
-        {
             m_TeleportSpots[extraTeleportSpots.Length + i] = autoTravelSpots[i];
-        }
 
 
         // Debug log to check if fast travel spots are initialized.
@@ -203,18 +192,15 @@ public class WristUIPauseManage : MonoBehaviour
     }
 
     /// <summary>
-    /// A method to enact the fast travel functinionality where the user
-    /// actually teleports to the intended spots(s). If only one spot is available,
-    /// the user gets teleported to that spot. If there are more than one spot available,
-    /// it cycles through the spots sequentially.
-    /// 
-    /// Contribution: jlgrijal(Jose)
-    /// Date: 03/06/2025
-    /// 
+    ///     A method to enact the fast travel functinionality where the user
+    ///     actually teleports to the intended spots(s). If only one spot is available,
+    ///     the user gets teleported to that spot. If there are more than one spot available,
+    ///     it cycles through the spots sequentially.
+    ///     Contribution: jlgrijal(Jose)
+    ///     Date: 03/06/2025
     /// </summary>
     public void EnactFastTravel()
     {
-
         // Making sure the VR user is assigned.
         if (vrUser == null)
         {
@@ -248,13 +234,9 @@ public class WristUIPauseManage : MonoBehaviour
         // Teleporting to the new position coodrinate to add
         // a marginal offset to make sure the movement occurs.
         vrUser.position = intendedPosition + new Vector3(0, 0.5f, 0); // Adding marginal offset to
-                                                                      // to make sure movement occurs.
+        // to make sure movement occurs.
 
         // Re-enabling the Character Controller after teleportation.
-        if (cc != null)
-        {
-            cc.enabled = true;
-        }
-
+        if (cc != null) cc.enabled = true;
     }
 }
